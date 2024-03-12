d4f567b2e5e943959d6af7c003ba8317e268df1c
Crowdbotics <engineering@crowdbotics.com>
2024-03-12 17:56:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "new-app-eduardo-de-134286",
+  url: "https://new-app-eduardo-de-134286.botics.co",
+  api: "https://new-app-eduardo-de-134286.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
